7c2a726cfb6b23305f2e1ee262b038d21f46b5b7
Nyanjui <nyanjuiarthur@gmail.com>
2026-03-23 10:59:28 +0300

    Complete the class and method demos for session 04

---
+++ sess04_classes_and_methods/RightAngledTriangle.cs
@@ -0,0 +1,57 @@
+﻿namespace sess04_classes_and_methods;
+
+/// <summary>
+/// This class is used to create Right-angled triangle objects.
+/// </summary>
+public class RightAngledTriangle
+{
+    // Private instance variables
+    private byte height;
+    private byte @base;
+    private byte hypotenuse;
+
+    /// <summary>
+    /// Create a Right-angled triangle and initialises all its sides to 0
+    /// </summary>
+    public RightAngledTriangle()
+    {
+        this.height = 0;
+        this.@base = 0;
+        this.hypotenuse = 0;
+    }
+
+    /// <summary>
+    /// Creates/instantiates a Right-Angled triangle with the height and base passed in as parameters.
+    /// The hypotenuse is calculated using Pythagoras theorem.
+    /// </summary>
+    /// <param name="height">The triangle's height</param>
+    /// <param name="base">The triangle's base</param>
+    public RightAngledTriangle(byte height, byte @base)
+    {
+        this.height = height;
+        this.@base = @base;
+        this.hypotenuse = (byte)(Math.Sqrt(height * height + @base * @base));
+        // this.hypotenuse = (byte)(Math.Sqrt(Math.pow(height, 2) + Math.pow(@base,2)));
+    }
+
+    private int CalculateArea()
+    {
+        return (int) (.5 * this.@base * this.height);
+        // not the same as 1/2 * this.@base * this.height
+    }
+
+    private int CalculatePerimeter()
+    { return this.@base + this.height + this.hypotenuse; }
+
+    public override string ToString()
+    {
+        return $"Right-Angled Triangle's Dimensions" +
+               $"\n{new string('-',50)}" +
+               $"\nBase: {this.@base}" +
+               $"\nHeight: {this.height}" +
+               $"\nHypotenuse: {this.hypotenuse}" +
+               $"\nArea: {this.CalculateArea()}" +
+               $"\nPerimeter: {this.CalculatePerimeter()}" +
+               $"\n{new string('-',50)}\n";
+    }
+}
---
+++ sess04_classes_and_methods/SimplePerson.cs
@@ -0,0 +1,58 @@
+﻿namespace sess04_classes_and_methods;
+
+/// <summary>
+/// The SimplePerson class will be used to create/instantiate Person objects and later inherited to
+/// create specialised classes.
+/// </summary>
+
+public class SimplePerson
+{
+    // Instance fields
+    protected string names;
+    protected DateTime birthDate;
+    protected string gender;
+    protected string nationality;
+
+    public SimplePerson()
+    {
+        this.names = string.Empty;
+        this.birthDate = DateTime.MinValue;
+        this.gender = string.Empty;
+        this.nationality = string.Empty;
+    }
+
+    public string GetNames()
+    {
+        return this.names;
+    }
+    public void SetNames(string names) {this.names = names;}
+
+    public DateTime GetBirthDate()
+    {
+        return this.birthDate;
+    }
+    public void SetBirthDate(DateTime birthDate) {this.birthDate = birthDate;}
+
+    public string GetGender()
+    {
+        return this.gender;
+    }
+    public void SetGender(string gender){this.gender = gender;}
+
+    public string GetNationality()
+    {
+        return this.nationality;
+    }
+    public void SetNationality(string nationality){this.nationality = nationality;}
+
+    public override string ToString()
+    {
+        return $"Person's Details" +
+               $"\n----------------------------------------" +
+               $"\nName: {this.GetNames()}" +
+               $"\nDate of Birth: {this.GetBirthDate()}" +
+               $"\nGender: {this.GetGender()}" +
+               $"\nNationality: {this.GetNationality()}" +
+               $"\n----------------------------------------\n";
+    }
+}
