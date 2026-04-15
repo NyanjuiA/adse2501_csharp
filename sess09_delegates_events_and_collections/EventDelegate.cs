a831429489d82af6cb7f9eb34d277bee5025d3e3
Nyanjui <nyanjuiarthur@gmail.com>
2026-04-15 10:42:45 +0300

    Introduce C# delegates

---
+++ sess09_delegates_events_and_collections/EventDelegate.cs
@@ -0,0 +1,54 @@
+﻿namespace sess09_delegates_events_and_collections;
+
+/// <summary>
+/// Program to demonstrate how to work with delegates and events.
+/// </summary>
+
+// Define the delegate for the event handler outside the class
+public delegate void MyEventHandler(string value);
+public class EventDelegate
+{
+    static void Main(string[] args)
+    {
+        // Declare and instantiate an EventPublisher object
+        EventPublisher eventPub = new EventPublisher();
+        // Assign the EventPublisher object to the UserStringChanged method
+        eventPub.valueChanged += UserStringChanged;
+
+        // String variable to hold the string/input fromthe user
+        string userString;
+        do
+        {
+            // Prompt the user for a string
+            Console.WriteLine("Please enter some text or type 'exit' to" +
+                              "end/quit the application.");
+            userString = Console.ReadLine();
+            // Display the string if it's not exit
+            if (userString.ToUpper().Equals("EXIT"))
+                eventPub.Value = userString;
+        } while (!userString.ToLower().Equals("exit"));
+        Console.WriteLine("Goodbye!");
+    }
+}
+
+/// <summary>
+/// Class that will be used to publish events
+/// </summary>
+internal class EventPublisher
+{
+    private string _val;
+
+    // Declare the event
+    public event MyEventHandler valueChanged;
+
+    public string Value
+    {
+        set
+        {
+            // Set the value of the private string '_val' to the string passed in
+            this._val = value;
+            // When the string value changes, fire/trigger the event
+            this.valueChanged(_val);
+        }
+    }
+}
